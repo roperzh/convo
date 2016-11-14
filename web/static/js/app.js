903c6d75a6bd488fd7df1cafbf96a49f16164008
Roberto Dip <dip.jesusr@gmail.com>
2016-11-14 15:43:15 -0300

    Bulk import styles and JS files

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,47 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+
+var dots=[];
+var numDots = 100;
+var width = window.innerWidth - 30;
+var height = window.innerHeight - 30;
+
+for(var i=0 ; i<numDots ; i++){
+  dots.push({
+    x : Math.random() * width,
+    y : Math.random() * height
+  })
+}
+
+var validColours = {
+  1: "rgb(87, 141, 200)",
+  2: "rgb(233, 103, 94)",
+  3: "rgb(104, 197, 153)",
+  4: "rgb(175, 62, 192)",
+  5: "rgb(230, 228, 228)"
+}
+
+function draw() {
+  var div = document.createElement('div')
+  div.style.overflow = "hidden";
+
+  var j, dot;
+  for(j = 0; j < numDots; j++) {
+    var dot = dots[j];
+    var color = validColours[Math.floor(Math.random()*5)];
+
+    var span = document.createElement('span');
+    span.className = "dot";
+    span.style.backgroundColor = color;
+    span.style.top = dot.y + "px";
+    span.style.left = dot.x + "px";
+
+    div.appendChild(span);
+  }
+
+  document.body.appendChild(div);
+}
+
+draw()
