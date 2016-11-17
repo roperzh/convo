1caa59a33d5d6ccad83ec821a4ae91efd2425d89
Roberto Dip <dip.jesusr@gmail.com>
2016-11-17 00:12:16 -0300

    Play around w/Elixir Task and Supervisor

--- lib/convo.ex
+++ lib/convo.ex
@@ -10,14 +10,15 @@ defmodule Convo do
     children = [
       # Start the endpoint when the application starts
       supervisor(Convo.Endpoint, []),
-      supervisor(Task.Supervisor, [[name: Convo.MP3Uploader, restart: :transient]])
+      worker(Task.Supervisor, [[name: Convo.MP3Uploader, restart: :transient, strategy: :one_for_one, max_seconds: 1, max_restarts: 10]])
       # Start your own worker by calling: Convo.Worker.start_link(arg1, arg2, arg3)
       # worker(Convo.Worker, [arg1, arg2, arg3]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Convo.Supervisor]
+    opts = [strategy: :one_for_one, name: Convo.Supervisor, max_seconds: 30, max_restarts: 10]
+
     Supervisor.start_link(children, opts)
   end
 
